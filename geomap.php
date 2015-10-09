1f9f3e92a28fec814fc1f5bcabb99e25789003fe
Darrrragh <Darrrragh@users.noreply.github.com>
2015-10-09 18:43:41 +0100

    Create geomap.php

---
+++ geomap.php
@@ -0,0 +1,36 @@
+<html>
+ <head>
+  <title>Offer App - GeoMap</title>
+  <meta charset="utf-8"/>
+  <link href="style/style.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="style/bootstrap.min.css" />
+  <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+  <script src="js/jquery-1.11.3.min.js"></script>
+  <script src="js/geomap.js"></script>
+  <script src="js/bootstrap.js"></script>
+ </head>
+ <body>
+	<div class="navbar-static-top navbar-inverse" id="home">
+		<div class="container">
+			<div class="navbar-brand">
+				Offer App
+			</div>
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+				Menu
+			</button>
+			<div class="collapse navbar-collapse navHeaderCollapse navbar-left">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<center>
+		
+<p id="map"></p>
+
+	</center>
+ </body>
+</html>
